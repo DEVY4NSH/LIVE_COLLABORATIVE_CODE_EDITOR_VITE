a76b9c57c1b08ad344ad511c7fa3344c3c97bbfc
DEVY4NSH <101127637+DEVY4NSH@users.noreply.github.com>
2024-06-14 22:54:47 +0530

    index.js

--- server.jsx
+++ server.jsx
@@ -8,7 +8,10 @@ const ACTIONS = require('./src/serverActions.js');
 const server = http.createServer(app);
 const io = new Server(server);
 
+
+
 app.use(express.static('build'));
+
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
